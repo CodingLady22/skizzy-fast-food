1bb7c4fb5a120fcb982571e7fab30b475364b918
Maye Jesuorobo <nayawes@yahoo.com>
2024-01-27 00:56:51 +0100

    Take out link to menu section below

--- skizzy-fast-food/src/components/Header.jsx
+++ skizzy-fast-food/src/components/Header.jsx
@@ -1,12 +1,14 @@
 import { Link } from "react-router-dom"
+
 function Header() {
     return (
         <header className="bg-img d-flex flex-column justify-content-between" id="home">
             <div></div>
             <div className="d-flex flex-column text-center">
                 {/* when using logo, remember to change 'text-center' to 'justfy-content-center */}
-                <span className="display-2">More <br /> Than a Meal!</span>
-                <p><Link to="/menu" className="btn btn-lg btn-dark hover-btn fs-4">View Menu</Link></p>
+                <span className="display-2 text-black fw-medium">More <br /> Than a Meal!</span>
+                {/* Make border thicker */}
+                {/* <p><Link to="/menu" className="btn btn-outline-dark text-white hover-btn fs-4">View Menu</Link></p> */}
             </div>
             <div className="d-flex">
                 <span className="bg-dark text-white p-3 h2">Open from 10am to 11pm - Call 333-456-9999</span>
