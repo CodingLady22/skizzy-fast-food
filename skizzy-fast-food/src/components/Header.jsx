749544c870c1635e6bb4fdeed7612be21405d1e3
Maye Jesuorobo <nayawes@yahoo.com>
2024-01-26 21:23:07 +0100

    comment added

--- skizzy-fast-food/src/components/Header.jsx
+++ skizzy-fast-food/src/components/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
         <header className="bg-img d-flex flex-column justify-content-between" id="home">
             <div></div>
             <div className="d-flex flex-column text-center">
-                {/* if using logo, remember to change 'text-center' to 'justfy-content-center */}
+                {/* when using logo, remember to change 'text-center' to 'justfy-content-center */}
                 <span className="display-2">More <br /> Than a Meal!</span>
                 <p><Link to="/menu" className="btn btn-lg btn-dark hover-btn fs-4">View Menu</Link></p>
             </div>
