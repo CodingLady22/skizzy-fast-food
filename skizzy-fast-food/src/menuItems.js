f511dcec464d5f23e40d848dae036c171ffb1d7f
Maye Jesuorobo <nayawes@yahoo.com>
2024-01-29 00:07:23 +0100

    Add the menu temporarily to a file that will later be transfered to the database

---
+++ skizzy-fast-food/src/menuItems.js
@@ -0,0 +1,173 @@
+const menuItems = [
+    {
+        id: "1",
+        category: "Burgers",
+        name: "Classic Burger",
+        ingredients: "Beef patty, lettuce, tomato, cheese, pickles, mayo",
+        price: "3.99"
+    },
+    {
+        id: "2",
+        category: "Burgers",
+        name: "BBQ Bacon Burger",
+        ingredients: "Beef patty, bacon, cheddar cheese, lettuce, tomato, BBQ sauce",
+        price: "5.99"
+    },
+    {
+        id: "3",
+        category: "Burgers",
+        name: "Spicy Jalapeno Burger",
+        ingredients: "Beef patty, jalapenos, pepper jack cheese, lettuce, tomato, spicy mayo",
+        price: "4.99"
+      },
+      {
+        id: "4",
+        category: "Burgers",
+        name: "Mushroom Swiss Burger",
+        ingredients: "Beef patty, sautéed mushrooms, swiss cheese, lettuce, tomato, mayo",
+        price: "6.99"
+      },
+      {
+        id: "5",
+        category: "Burgers",
+        name: "Double Cheeseburger",
+        ingredients: "Two beef patties, American cheese, lettuce, pickles, special sauce",
+        price: "7.99"
+      },
+    {
+        id: "6",
+        category: "Wraps",
+        name: "Chicken Caesar Wrap",
+        ingredients: "Grilled chicken, romaine lettuce, parmesan cheese, Caesar dressing",
+        price: "7.99"
+    },
+    {
+        id: "7",
+        category: "Wraps",
+        name: "Classic Beef Wrap",
+        ingredients: "Grilled beef, lettuce, parmesan cheese, Caesar dressing",
+        price: "8.99"
+    },
+    {
+        id: "8",
+        category: "Wraps",
+        name: "Vegetarian Wrap",
+        ingredients: "Grilled vegetables, hummus, feta cheese, spinach, tzatziki",
+        price: "6.99"
+      },
+      {
+        id: "9",
+        category: "Wraps",
+        name: "Buffalo Chicken Wrap",
+        ingredients: "Buffalo chicken, lettuce, cheddar cheese, ranch dressing",
+        price: "8.99"
+      },
+      {
+        id: "10",
+        category: "Wraps",
+        name: "Turkey Club Wrap",
+        ingredients: "Turkey, bacon, lettuce, tomato, mayo, Swiss cheese",
+        price: "9.99"
+      },
+    {
+        id: "11",
+        category: "Combos",
+        name: "Big Skizzy",
+        ingredients: "Classic Burger, fries, drink",
+        price: "9.99"
+    },
+    {
+        id: "12",
+        category: "Combos",
+        name: "kiddy Skizzy",
+        ingredients: "Nuggets, fries, drink",
+        price: "9.99"
+    },
+    {
+        id: "13",
+        category: "Combos",
+        name: "Skizzy Deluxe Combo",
+        ingredients: "Classic Burger, sweet potato fries, milkshake",
+        price: "12.99"
+    },
+    {
+        id: "14",
+        category: "Sides",
+        name: "Garlic Parmesan Fries",
+        ingredients: "Crispy fries, garlic, parmesan, herbs",
+        price: "4.99"
+    },
+    {
+        id: "15",
+        category: "Sides",
+        name: "Bacon Cheese Fries",
+        ingredients: "Crispy fries, bacon, parmesan",
+        price: "2.99"
+    },
+    {
+        id: "16",
+        category: "Sides",
+        name: "Sweet Potato Fries",
+        ingredients: "Crispy sweet potato fries, sea salt, cinnamon",
+        price: "4.99"
+      },
+      {
+        id: "17",
+        category: "Sides",
+        name: "Onion Rings",
+        ingredients: "Battered and fried onion rings, chipotle mayo",
+        price: "3.99"
+      },
+      {
+        id: "18",
+        category: "Sides",
+        name: "Cheese Stuffed Jalapenos",
+        ingredients: "Jalapenos stuffed with cream cheese, breaded and fried",
+        price: "5.99"
+      },
+      {
+        id: "19",
+        category: "Sides",
+        name: "Loaded Nachos",
+        ingredients: "Tortilla chips, melted cheese, black beans, guacamole, sour cream",
+        price: "6.99"
+      },
+      {
+        id: "20",
+        category: "Drinks",
+        name: "Iced Coffee",
+        ingredients: "Cold brewed coffee, ice, milk, sugar",
+        price: "3.50"
+      },
+      {
+        id: "21",
+        category: "Drinks",
+        name: "Mango Smoothie",
+        ingredients: "Mango, yogurt, honey, ice",
+        price: "4.50"
+      },
+      {
+        id: "22",
+        category: "Drinks",
+        name: "Peach Lemonade",
+        ingredients: "Freshly squeezed lemonade, peach syrup",
+        price: "2.99"
+      },
+      {
+        id: "23",
+        category: "Drinks",
+        name: "Strawberry Basil Infusion",
+        ingredients: "Strawberries, basil, sparkling water",
+        price: "3.99"
+      },
+    {
+        id: "24",
+        category: "Drinks",
+        name: "Bottled Water",
+        ingredients: "water",
+        price: "1.50"
+    }
+]
+
+
+export default menuItems
