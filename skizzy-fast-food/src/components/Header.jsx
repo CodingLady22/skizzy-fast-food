b290cccc7b6a4c13b6539b49ce4ffd9447ece002
Maye Jesuorobo <nayawes@yahoo.com>
2024-01-31 01:56:06 +0100

    Add the menu id to the bottom of the header so the navbar stops just before the menu section starts

--- skizzy-fast-food/src/components/Header.jsx
+++ skizzy-fast-food/src/components/Header.jsx
@@ -11,7 +11,7 @@ function Header() {
                 {/* <p><Link to="/menu" className="btn btn-outline-dark text-white hover-btn fs-4">View Menu</Link></p> */}
             </div>
             <div className="d-flex">
-                <span className="bg-dark text-white p-3 h2">Open from 10am to 11pm - Call 333-456-9999</span>
+                <span className="bg-dark text-white p-3 h2" id='menu'>Open from 10am to 11pm - Call 333-456-9999</span>
             </div>
         </header>
     )
