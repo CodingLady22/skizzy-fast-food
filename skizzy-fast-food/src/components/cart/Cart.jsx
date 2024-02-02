489a727b24a54127d803a8b428d41c28255e4bac
Maye Jesuorobo <nayawes@yahoo.com>
2024-02-02 23:19:54 +0100

    take out span to display number of cart items for styling purposes.

--- skizzy-fast-food/src/components/cart/Cart.jsx
+++ skizzy-fast-food/src/components/cart/Cart.jsx
@@ -2,11 +2,8 @@ import CartIcon from "./CartIcon"
 
 function Cart() {
   return (
-    <button className="btn btn-outline-success d-flex justify-content-around rounded-pill">
-        <span className="p-2"><CartIcon /></span>
-        <span className="p-2 fw-bold">Your Cart</span>
-        {/* Make number rounded and fixed on the cart if possible */}
-        <span className="border border-success-subtle p-2 text-success bg-success-subtle rounded">4</span>
+    <button className="btn btn-outline-success me-3 d-flex justify-content-around rounded-pill">
+        <span className=""><CartIcon /></span>
     </button>
   )
 }
