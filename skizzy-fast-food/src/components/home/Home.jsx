26d8fd9ee0695a60d19010fbfcec5b338e2c7d37
Maye Jesuorobo <nayawes@yahoo.com>
2024-02-04 19:50:55 +0100

    transfer all files on the home page to a file called home

---
+++ skizzy-fast-food/src/components/home/Home.jsx
@@ -0,0 +1,18 @@
+
+import Nav from "./Nav";
+import Header from "./Header";
+import Menu from "./Menu";
+import About from "./About";
+
+function Home() {
+  return (
+    <div>
+        <Nav />
+        <Header />
+        <Menu />
+        <About />
+    </div>
+  )
+}
+
+export default Home
