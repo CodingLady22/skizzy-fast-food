687f17c33373496b3588c0499214013dd4c3d8a6
Maye Jesuorobo <nayawes@yahoo.com>
2024-02-10 14:42:40 +0100

    Initial styling of the login page

--- skizzy-fast-food/src/components/auth/Login.jsx
+++ skizzy-fast-food/src/components/auth/Login.jsx
@@ -9,33 +9,39 @@ function Login() {
     }
 
   return (
-    <div className='container-fluid'>
-        <h2>Login</h2>
-        <form action="/login" method='post' className='container'>
-            <label htmlFor="email" className='form-label'>Email address</label>
-            <input 
-            type="email"
-            className='form-control'
-            placeholder='name@example.com'
+    <section className="login-section py-5">
+        <div className="login-page">
+        <form action="/login" method='post' className="mx-auto my-5">
+
+            <h2 className="text-center">Login</h2>
+            <div className="mb-4 mt-5">
+                <label htmlFor="email" className="form-label">Email Address</label>
+                <input 
+                type="email" 
+                className="form-control" 
+                id="exampleInputEmail1" 
+                aria-describedby="emailHelp" 
+                />
+            </div>
+            <div className="mb-4">
+                <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+                <input 
+                type="password" 
+                className="form-control" 
+                id="exampleInputPassword1" 
+                />
+                <div id="emailHelp" className="form-text">Forget password?</div>
+            </div>
             
-            />
-            <input 
-            type="password"
-            className='form-control'
-            placeholder="Password" 
-            />
-            <Link to="/">
-                    <button 
-                    className="login-button" 
-                    onClick={handleClick} 
-                    type="submit">Sign in</button>
-            </Link>
-        </form>
-        <p>Not a member? 
+            <button type="submit" className="login-btn">Login</button>
+            <p className="mt-3 text-center">Not a member? 
             {/* Change to Link element */}
-                <a href="#">Register</a>
+                <a href="#" className="mx-1">Sign up</a>
             </p>
+        </form>
+        
     </div>
+    </section>
   )
 }
 
