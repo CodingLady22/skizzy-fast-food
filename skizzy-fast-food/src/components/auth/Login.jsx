8aec5592ab355e1cc4d3a552472f35ba6039e7a3
Maye Jesuorobo <nayawes@yahoo.com>
2024-02-11 01:20:33 +0100

    add link to sign up page

--- skizzy-fast-food/src/components/auth/Login.jsx
+++ skizzy-fast-food/src/components/auth/Login.jsx
@@ -11,7 +11,7 @@ function Login() {
   return (
     <section className="login-section py-5">
         <div className="login-page">
-        <form action="/login" method='post' className="mx-auto my-5">
+        <form action="/login" method='post' className="formSection mx-auto my-5">
 
             <h2 className="text-center">Login</h2>
             <div className="mb-4 mt-5">
@@ -19,7 +19,6 @@ function Login() {
                 <input 
                 type="email" 
                 className="form-control" 
-                id="exampleInputEmail1" 
                 aria-describedby="emailHelp" 
                 />
             </div>
@@ -27,16 +26,14 @@ function Login() {
                 <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
                 <input 
                 type="password" 
-                className="form-control" 
-                id="exampleInputPassword1" 
+                className="form-control"
                 />
                 <div id="emailHelp" className="form-text">Forget password?</div>
             </div>
             
             <button type="submit" className="login-btn">Login</button>
             <p className="mt-3 text-center">Not a member? 
-            {/* Change to Link element */}
-                <a href="#" className="mx-1">Sign up</a>
+                <Link to="/signup" className="form-link mx-1">Sign up</Link>
             </p>
         </form>
         
