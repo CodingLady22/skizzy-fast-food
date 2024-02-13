b2fdb2f7ed137d831fa339e0875570f17c25eb4b
Maye Jesuorobo <nayawes@yahoo.com>
2024-02-13 18:56:55 +0100

    wrap the router in the context provider

--- skizzy-fast-food/src/App.jsx
+++ skizzy-fast-food/src/App.jsx
@@ -1,25 +1,28 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import '../src/scss/styles.scss'
 import Home from './components/home/Home'
 import Login from './components/auth/Login'
 import SignUp from './components/auth/SignUp'
 import CartPage from './components/cart/CartPage'
 import Nav from './components/home/Nav'
+import { CartContextProvider } from './context/cartContextProvider'
 import './App.css'
+import '../src/scss/styles.scss'
 
 function App() {
 
   return (
     <>
-      <Router>
-        <Nav />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/signup' element={<SignUp />} />
-          <Route path='/cartPage' element={<CartPage />} />
-        </Routes>
-      </Router>
+      <CartContextProvider>
+        <Router>
+          <Nav />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/signup' element={<SignUp />} />
+            <Route path='/cartPage' element={<CartPage />} />
+          </Routes>
+        </Router>
+      </CartContextProvider>
     </>
   )
 }
