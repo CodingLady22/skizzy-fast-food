fc9dd1c44fea05c1d93f822e7c5a45cf11412400
Maye Jesuorobo <nayawes@yahoo.com>
2024-02-27 21:22:56 +0100

    playing around with styling

--- skizzy-fast-food/src/components/home/Header.jsx
+++ skizzy-fast-food/src/components/home/Header.jsx
@@ -2,11 +2,11 @@
 
 function Header() {
     return (
-        <header className="bg-img d-flex flex-column justify-content-between" id="home">
+        <header className="bg-black bg-img d-flex flex-column justify-content-between" id="home">
             <div></div>
             <div className="d-flex flex-column text-start">
                 {/* Change 'text-center' to 'justfy-content-center if using the logo*/}
-                <span className="display-2 fw-bold ms-5" style={{fontFamily: "Alumni Sans", color : "#141414"}}>More <br /> Than a Meal!</span>
+                <span className="display-2 fw-bold ms-5" style={{fontFamily: "Alumni Sans", color : "#fff"}}>More <br /> Than a Meal!</span>
             </div>
             <div className="d-flex">
                 <span className="bg-dark text-white p-3 h2" id='menu'>Open from 10am to 11pm - Call 333-456-9999</span>
