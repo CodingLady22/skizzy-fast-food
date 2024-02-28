37a317b2232964f2ffd8fe06862e8cd113dd6326
Maye Jesuorobo <nayawes@yahoo.com>
2024-02-28 15:43:56 +0100

    Create the component to collect the delivery address

--- skizzy-fast-food/src/App.jsx
+++ skizzy-fast-food/src/App.jsx
@@ -1,30 +1,29 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Home from './components/home/Home'
-import Login from './components/auth/Login'
-import SignUp from './components/auth/SignUp'
-import CartPage from './components/cart/CartPage'
-import Nav from './components/home/Nav'
-import { CartContextProvider } from './context/cartContextProvider'
-import './App.css'
-import '../src/scss/styles.scss'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/home/Home";
+import Login from "./components/auth/Login";
+import SignUp from "./components/auth/SignUp";
+import CartPage from "./components/cart/CartPage";
+import Nav from "./components/home/Nav";
+import { CartContextProvider } from "./context/cartContextProvider";
+import "./App.css";
+import "../src/scss/styles.scss";
 
 function App() {
-
   return (
     <>
       <CartContextProvider>
         <Router>
           <Nav />
           <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/login' element={<Login />} />
-            <Route path='/signup' element={<SignUp />} />
-            <Route path='/cartPage' element={<CartPage />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/cartPage" element={<CartPage />} />
           </Routes>
         </Router>
       </CartContextProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
